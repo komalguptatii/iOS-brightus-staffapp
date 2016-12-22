626f74bc34d5b1f847c3d45ebe072db672602400
Komal Gupta <komalgupta@komal-gupta.local>
2016-12-22 12:21:23 +0530

    Attendance Detail TableView

--- BrightUs_StaffAttendance/DetailsViewController.swift
+++ BrightUs_StaffAttendance/DetailsViewController.swift
@@ -10,12 +10,19 @@ import Foundation
 import UIKit
 
 /// Attendance Details 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
+    @IBOutlet var detailTableView: UITableView!
+    
+    var attendanceDetailArray = NSMutableArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,5 +30,50 @@ class DetailsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-       
+    //MARK: - TableView Delegate & Datasource
+    //MARK: - 
+    
+    /**
+     Number of Sections in TableView
+     
+     - parameter return : number of sections
+ 
+    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /**
+     Number of Rows in Section 
+     
+     - parameter return : number of rows in one section
+ 
+    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return attendanceDetailArray.count
+        return 2
+        
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
+    }
+    
+    /**
+    Definition of Cell
+     
+     - parameter return : TiiAttendanceDetailCell 
+     
+     - parameter defined_Terms : particularDate, checkinTime, checkOutTime, totalTime
+ 
+    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TiiAttendanceDetailCell
+        
+        return cell
+    }
+
 }
